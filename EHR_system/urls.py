22585058206f6b6d3a15673a8f936b861ea1d43f
Tanmaya <sharmatanmay26062007@gmail.com>
2026-07-19 10:40:10 +0530

    Fix redirect URL to match accounts prefix

--- EHR_system/urls.py
+++ EHR_system/urls.py
@@ -8,7 +8,7 @@ from django.urls import path, include
 from django.views.generic import RedirectView
 
 urlpatterns = [
-    path('', RedirectView.as_view(url='/login/', permanent=False)),
+   path('', RedirectView.as_view(url='/accounts/login/', permanent=False)),
     path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls')),
     path('dashboard/', include('dashboard.urls')),
